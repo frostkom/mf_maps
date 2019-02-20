cf02b53fd5576b6c812f1aa7d6bb5fc2e13bc1d3
frostkom <martin.fors@frostkom.se>
2019-02-20 19:49:14 +0100

    Make it possible to fetch coordinates from location/address

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,41 @@
 <?php
 
+function get_coordinates_from_location($location)
+{
+	$out = "";
+
+	$setting_gmaps_api = get_option('setting_gmaps_api');
+
+	if($location != '' && $setting_gmaps_api != '')
+	{
+		list($content, $headers) = get_url_content(array(
+			'url' => "https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($location)."&key=".$setting_gmaps_api,
+			'catch_head' => true,
+		));
+
+		switch($headers['http_code'])
+		{
+			case 200:
+				$json = json_decode($content, true);
+
+				if($json['results'][0]['geometry'])
+				{
+					$lat = str_replace(',', '.', $json['results'][0]['geometry']['location']['lat']);
+					$lng = str_replace(',', '.',$json['results'][0]['geometry']['location']['lng']);
+
+					$out = "(".$lat.", ".$lng.")";
+				}
+			break;
+
+			default:
+				do_log(__("I could not connect to gMaps", 'lang_maps').": ".$headers['http_code']." (".var_export($headers, true).", ".$content.")");
+			break;
+		}
+	}
+
+	return $out;
+}
+
 function get_map($data)
 {
 	if(!isset($data['id'])){			$data['id'] = mt_rand(1, 100);}
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Maps
 Plugin URI: https://github.com/frostkom/mf_maps
 Description: 
-Version: 2.5.1
+Version: 2.5.2
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
