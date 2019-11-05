bf1aba01464d454aa4fd69f137582df5298fdd11
frostkom <martin.fors@frostkom.se>
2019-11-05 14:40:13 +0100

    Remove old DB changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Maps
 Plugin URI: https://github.com/frostkom/mf_maps
 Description: 
-Version: 2.5.10
+Version: 2.5.12
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -18,11 +18,8 @@ include_once("include/classes.php");
 
 $obj_maps = new mf_maps();
 
-add_action('cron_base', 'activate_maps', mt_rand(1, 10));
-
 if(is_admin())
 {
-	register_activation_hook(__FILE__, 'activate_maps');
 	register_uninstall_hook(__FILE__, 'uninstall_maps');
 
 	add_action('admin_init', array($obj_maps, 'settings_maps'));
@@ -30,7 +27,6 @@ if(is_admin())
 
 	add_action('show_user_profile', array($obj_maps, 'edit_user_profile'));
 	add_action('edit_user_profile', array($obj_maps, 'edit_user_profile'));
-	//add_action('personal_options_update', array($obj_maps, 'edit_user_profile_update'));
 	add_action('profile_update', array($obj_maps, 'profile_update'));
 }
 
@@ -44,15 +40,7 @@ else
 add_filter('get_map', array($obj_maps, 'get_map'), 10, 2);
 add_filter('get_coordinates_from_location', array($obj_maps, 'get_coordinates_from_location'));
 
-function activate_maps()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_maps_height'),
-	));
-
-	replace_user_meta(array('old' => 'profile_search_input', 'new' => 'meta_search_input'));
-	replace_user_meta(array('old' => 'profile_search_coords', 'new' => 'meta_search_coords'));
-}
+load_plugin_textdomain('lang_maps', false, dirname(plugin_basename(__FILE__)).'/lang/');
 
 function uninstall_maps()
 {
@@ -61,5 +49,3 @@ function uninstall_maps()
 		'meta' => array('meta_search_input', 'meta_search_coords'),
 	));
 }
-
-load_plugin_textdomain('lang_maps', false, dirname(plugin_basename(__FILE__)).'/lang/');
