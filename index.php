5c01025c7c9e2c43e3c93fae47b6dfa4d161dad4
frostkom <martin.fors@frostkom.se>
2025-03-26 10:11:22 +0100

    Remove DK and NO

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Maps
 Plugin URI: https://github.com/frostkom/mf_maps
 Description:
-Version: 2.6.6
+Version: 2.6.7
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
