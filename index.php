99bba86805fc648b15c0d2fc5e2da2382699fa08
Martin Fors <martin.fors@frostkom.se>
2025-07-19 11:45:06 +0200

    Remove Depends and GitHub URI

--- index.php
+++ index.php
@@ -3,15 +3,12 @@
 Plugin Name: MF Maps
 Plugin URI: https://github.com/frostkom/mf_maps
 Description:
-Version: 2.6.10
+Version: 2.6.11
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
 Text Domain: lang_maps
 Domain Path: /lang
-
-Depends: MF Base
-GitHub Plugin URI: frostkom/mf_maps
 */
 
 if(!function_exists('is_plugin_active') || function_exists('is_plugin_active') && is_plugin_active("mf_base/index.php"))
